5ad8f33012340bd2ca4c3ea09b405183692db9fc
Anton Solovyov <antonsolovyov@gmail.com>
2024-12-15 17:40:56 +0000

    App setup

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+
+function App() {
+  const account = useAccount()
+  const { connectors, connect, status, error } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  return (
+    <>
+      <div>
+        <h2>Account</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          addresses: {JSON.stringify(account.addresses)}
+          <br />
+          chainId: {account.chainId}
+          <br />
+          chain: {account.chain?.name}
+        </div>
+
+        {account.status === 'connected' && (
+          <button className="px-4 py-2 m-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" type="button" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        )}
+      </div>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            className="px-4 py-2 m-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </button>
+        ))}
+        <div>status: {status}</div>
+        <div>{error?.message}</div>
+      </div>
+    </>
+  )
+}
+
+export default App
